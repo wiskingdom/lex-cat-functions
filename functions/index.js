36fa0b3001cc1afe27ca3d43c80dd1850af38788
Yeonwoo Kim <wiskingdom@gmail.com>
2020-01-14 04:31:45 +0900

    Change function

--- functions/index.js
+++ functions/index.js
@@ -1,88 +1,58 @@
 const functions = require('firebase-functions');
 
 // subfunctions
-const getEntryStage = (isSkipped, needCheck, pos, sem) => {
-  if (needCheck) {
-    return 1;
-  } else if (!isSkipped && pos && sem && getters.semValid) {
-    return 3;
-  } else if (isSkipped) {
-    return 2;
-  } else {
-    return 0;
-  }
-};
 const getWorksetId = entryId => entryId.split('-').slice(0, 2).join('-');
-const getSuperEntryId = entryId => entryId.split('-').slice(0, 3).join('-');
 
 // common functions 
-exports.onEntryUpdate = functions.database
-  .ref('/dict/{domainName}/entries/{entryId}')
-  .onUpdate((change, context) => {
-    const { isSkipped, needCheck, pos, sem } = change.after.val();
-    const bf = change.before.val();
-    if (isSkipped === bf.isSkipped 
-      && needCheck === bf.needCheck 
-      && pos === bf.pos 
-      && sem === bf.sem ) {
-      return null;
-    }
-    const { entryId } = context.params;
-    const worksetId = getWorksetId(entryId);
-    const stage = getEntryStage(isSkipped, needCheck, pos, sem);
-    const updatedAt = Date.now();
-    const stateRef = change.after.ref.parent.parent
-      .child('entryStates').child(worksetId).child(entryId);
-
-    change.after.ref.child('updatedAt').set(updatedAt);
-    return stateRef.child('stage').set(stage);
-  });
-
-exports.onEntryStageUpdate = functions.database
-  .ref('/dict/{domainName}/entryStates/{worksetId}/{entryId}/stage')
+exports.onEntryMarkingUpdate = functions.database
+  .ref('/dict/{domainName}/entryMarkings/{worksetId}/{entryId}')
   .onUpdate((change, context) => {
     const { worksetId } = context.params;
-    const worksetStateRef = change.after.ref.parent.parent.parent.parent
-      .child('worksetStates').child(worksetId);
-    const worksetEntriesRef = change.after.ref.parent.parent;
-    return worksetEntriesRef
+    const { stage } = change.after.val();
+    const bf = change.before.val();
+    if (stage === bf.stage) {
+      return null;
+    }
+    const worksetRef = change.after.ref.parent.parent.parent
+      .child('worksets').child(worksetId);
+    const worksetMarkingsRef = change.after.ref.parent;
+    return worksetMarkingsRef
       .orderByChild('stage').startAt(2).once('value', snap => {
         const cntCompletes = snap.numChildren();
-        console.log(cntCompletes);
-        worksetStateRef.child('cntCompletes').set(cntCompletes);
+        console.log(`${worksetId}: ${cntCompletes} entries complete!`)
+        worksetRef.update({ cntCompletes });
       });
   });
 
-exports.onEntrySynsetUpdate = functions.database
-  .ref('/dict/{domainName}/entries/{entryId}/synset')
+exports.onEntrySynOfUpdate = functions.database
+  .ref('/dict/{domainName}/entries/{entryId}/synOf')
   .onUpdate((change, context) => {
-    const synset = change.after.val();
-    const bfSynset = change.before.val();
+    const synOf = change.after.val();
+    const bfSynOf = change.before.val();
+
     const { entryId } = context.params;
     const worksetId = getWorksetId(entryId);
-    const superEntryId = getSuperEntryId(entryId);
-    const stateRef = change.after.ref.parent.parent.parent
-      .child('entryStates').child(worksetId).child(entryId);
+    const entryRef = change.after.ref.parent;
+    const entryMarkingRef = change.after.ref.parent.parent.parent
+      .child('entryMarkings').child(worksetId).child(entryId);
     const synsetsRef = change.after.ref.parent.parent.parent
       .child('synsets');
-    const entryFreqRef = change.after.ref.parent.parent.parent
-      .child('superEntries').child(superEntryId).child('freq');
     const updatedAt = Date.now();
-    change.after.ref.parent.child('updatedAt').set(updatedAt);
-    stateRef.child('hasSynset').set(Boolean(synset));
+    entryRef.update({ updatedAt });
+    entryMarkingRef.update({ hasSynset: Boolean(synOf) });
 
-    if (synset && bfSynset) {
-      synsetsRef.child(bfSynset).child(entryId).remove();
-      return entryFreqRef.once('value', freqSnap => {
-        synsetsRef.child(synset).child(entryId).set(freqSnap.val());
+    if (synOf && bfSynOf) {
+      synsetsRef.child(bfSynOf).child(entryId).remove();
+      return entryRef.child('orthForm').once('value', snap => {
+        synsetsRef.child(synOf).child(entryId).set(snap.val());
       });
 
-    } else if (synset && !bfSynset) {
-      return entryFreqRef.once('value', freqSnap => {
-        synsetsRef.child(synset).child(entryId).set(freqSnap.val());
+    } else if (synOf && !bfSynOf) {
+      return entryRef.child('orthForm').once('value', snap => {
+        synsetsRef.child(synOf).child(entryId).set(snap.val());
       });
-    } else if (!synset && bfSynset) {
-      return synsetsRef.child(bfSynset).child(entryId).remove();
+    } else if (!synOf && bfSynOf) {
+      return synsetsRef.child(bfSynOf).child(entryId).remove();
     } else {
       return null;
     }
@@ -91,9 +61,8 @@ exports.onEntrySynsetUpdate = functions.database
 
 exports.onSynsetMemberDelete = functions.database
   .ref('/dict/{domainName}/synsets/{synsetId}/{memberId}')
-  .onDelete((snapshot, context) => {
+  .onDelete((snapshot) => {
     const synsetRef = snapshot.ref.parent;
-    const { synsetId } = context.params;
     return synsetRef.once('value', synSnap => {
       const synset = synSnap.val();
       if (!synset) {
@@ -103,16 +72,8 @@ exports.onSynsetMemberDelete = functions.database
         if (members.length === 1) {
           const theMemberId = members[0];
           return snapshot.ref.parent.parent.parent
-            .child('entries').child(theMemberId).child('synset')
+            .child('entries').child(theMemberId).child('synOf')
             .set('');
-        } else if (!synset[synsetId]) {
-          const newSynsetId = Object.entries(synset)
-            .sort((a, b) => Number(b[1]) - Number(a[1]))[0][0];
-          return members.forEach(member => {
-            snapshot.ref.parent.parent.parent
-              .child('entries').child(member).child('synset')
-              .set(newSynsetId);
-          });
         } else {
           return null;
         }
